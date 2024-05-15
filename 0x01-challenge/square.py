5474d01662f70f6e9b0cfbdffac1f5799a8bce00
Matumbai Binale <binalematumbai@gmail.com>
2024-05-16 00:43:44 +0300

    (task 1): Fix bug in area method and clean up

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
+""" Defines class Square. """
 
-class square():
-    
+
+class Square():
+    """ Represents a Square """
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ Initialize a Square """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
+        """ Area of a Square """
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def PerimiterOfMySquare(self):
+        """ Perimeter of a Square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String representation of a Square """
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+if __name__ == "__main__":
+    """ Driver code """
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.PerimiterOfMySquare())
